07e20a7373c7a65a04b04bcd7151932c4862d088
KARTHIKJINKALA <karthikjinkala11@gmail.com>
2024-11-18 11:21:30 +0530

    msg

---
+++ 13-11-2024-Modules/test1.js
@@ -0,0 +1,28 @@
+// function test(){
+//     console.log("testing")
+// }
+
+// function sum(a,b){
+//     console.log(a+b)
+// }
+// function mul(a,b){
+//     console.log(a*b)
+// }
+
+// var obj1={
+//     name:"karthik",
+//     age:21,
+//     city:"hyderabad"
+// }
+// var arr=[1,2,4,5]
+
+// test()
+// sum(7,3)
+// mul(2,6)
+
+// module.exports={
+    //     test,sum,mul,obj1,arr
+    // }
+//     var n1="karthik"
+
+// module.exports.name=n1
---
+++ 13-11-2024-Modules/test2.js
@@ -0,0 +1,7 @@
+
+// var out=require("./test1")
+
+// // out.test()
+// // out.sum(3,4)
+// // console.log(out.obj1["name"])
+// console.log(out.name)
