f73cd96fe4aa4e79716de8f88b252e5e286797ce
KARTHIKJINKALA <karthikjinkala11@gmail.com>
2024-11-24 23:31:10 +0530

    msg

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-var http=require("http")
+// var http=require("http")
 
 
-var server=http.createServer((req,res)=>{
+// var server=http.createServer((req,res)=>{
 
- res.write("Karthik jinkala 3ioojorfwoifs4")
- res.end()
+//  res.write("Karthik jinkala 3ioojorfwoifs4")
+//  res.end()
 
-})
+// })
 
-var port=3001
+// var port=3001
 
-server.listen(port,()=>{
-     console.log("server has been started at port"+"http://localhost:"+port)
-})
+// server.listen(port,()=>{
+//      console.log("server has been started at port"+"http://localhost:"+port)
+// })
