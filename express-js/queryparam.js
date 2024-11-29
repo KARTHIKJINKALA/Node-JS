c67f87a5f119b3963141eff405721b66ab60dbd7
KARTHIKJINKALA <karthikjinkala11@gmail.com>
2024-11-29 23:52:50 +0530

    msg

---
+++ express-js/queryparam.js
@@ -0,0 +1,18 @@
+// var express=require("express")
+// var app=express()
+
+// app.get("/products/:id",(req,res)=>{
+//     // console.log(req.query)
+//     // console.log(req.params)
+
+// res.send({
+//     params:req.params,
+//     query:req.query
+// })
+
+
+// })
+// var port=3004
+// app.listen(port,()=>{
+//   console.log("Server created succesfully")
+// })
