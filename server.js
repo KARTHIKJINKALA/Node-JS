8959b3671a6a50e47d33fed8f58289d9e403ff6b
KARTHIKJINKALA <karthikjinkala11@gmail.com>
2024-11-29 06:58:17 +0530

    msg

--- server.js
+++ server.js
@@ -12,6 +12,6 @@ var server=http.createServer((req,res)=>{
 var port=3003
 
 server.listen(port,()=>{
-     console.log("server has been started at port:"+"http://localhost:"+port)
+     console.log("server has been started succesfully:"+"http://localhost:"+port)
      
 })
