75290cb6b214a3b1c9d7928a5c7544a85e7ff30b
KARTHIKJINKALA <karthikjinkala11@gmail.com>
2024-11-29 22:00:47 +0530

    msg\

--- express1.js
+++ express1.js
@@ -1,11 +1,12 @@
 var express=require("express")
 var app=express()
+// console.log(app)
 
-app.post("/products/1",async(req,res)=>{
+app.get("/products/3",async(req,res)=>{
     var api=await fetch("https://fakestoreapi.com/products/")
     var response=await api.json()
 
-   res.send(response[0])
+   res.send(response[2])
 })
 var port=3003
 app.listen(port,()=>{
