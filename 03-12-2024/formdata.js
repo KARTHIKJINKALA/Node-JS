d72ba13da3d60cdb0a7f179c204a799f7bbd076d
KARTHIKJINKALA <karthikjinkala11@gmail.com>
2024-12-05 11:20:48 +0530

    msg

---
+++ 03-12-2024/formdata.js
@@ -0,0 +1,13 @@
+var express=require("express")
+var app=express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.post("/products",(req,res)=>{
+
+ res.send(req.body)
+})
+var port=3002
+app.listen(port,()=>{
+console.log("Server has been started succesfully")
+})
