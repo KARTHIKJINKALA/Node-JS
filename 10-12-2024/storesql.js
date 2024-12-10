bc1be254966351d9efd9ce18be072a1308d36a48
KARTHIKJINKALA <karthikjinkala11@gmail.com>
2024-12-10 18:28:56 +0530

    msg

--- 10-12-2024/storesql.js
+++ 10-12-2024/storesql.js
@@ -1,2 +1,34 @@
 var express=require("express")
 var app=express()
+var connect=require("./database")
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+app.post("/formdata",(req,res)=>{
+    var username=req.body.username
+    var password=req.body.password
+
+
+
+    console.log(req.body);
+    // var squery="CREATE TABLE NODESQL (USERNAME VARCHAR(20),PASSWORD VARCHAR(20))"
+
+var sinsert="INSERT INTO NODESQL (USERNAME,PASSWORD) VALUES(?,?)"
+
+    connect.query(sinsert,[username,password],(err,data) => {
+        if (err) {
+            console.error('Error executing query:', err);
+            return res.status(500).send('Database query failed.');
+        }
+        else{
+            res.send(req.body)
+        }
+    })
+    
+//    res.send("this is body data")
+})
+var port=3006
+app.listen(port,()=>{
+    console.log("Server has been created succesfully")
+})
