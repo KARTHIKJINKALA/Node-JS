86da84734edcf8bceefdfc3d55de91496b0d0d4d
KARTHIKJINKALA <karthikjinkala11@gmail.com>
2024-12-10 19:56:05 +0530

    msg

--- 10-12-2024/storesql.js
+++ 10-12-2024/storesql.js
@@ -1,34 +1,52 @@
-var express=require("express")
-var app=express()
-var connect=require("./database")
+var express = require("express");
+var app = express();
+var connect = require("./database");
 
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.post("/formdata",(req,res)=>{
-    var username=req.body.username
-    var password=req.body.password
+// app.post("/register", (req, res) => {
+//   var username = req.body.username;
+//   var password = req.body.password;
 
+//   console.log(req.body);
+//   // var squery="CREATE TABLE NODESQL (USERNAME VARCHAR(20),PASSWORD VARCHAR(20))"
 
+//   var sinsert = "INSERT INTO NODESQL (USERNAME,PASSWORD) VALUES(?,?)";
 
-    console.log(req.body);
-    // var squery="CREATE TABLE NODESQL (USERNAME VARCHAR(20),PASSWORD VARCHAR(20))"
+//   connect.query(sinsert, [username, password], (err, data) => {
+//     if (err) {
+//       console.error("Error executing query:", err);
+//       return res.status(500).send("Database query failed.");
+//     } else {
+//       res.send(req.body);
+//     }
+//   });
 
-var sinsert="INSERT INTO NODESQL (USERNAME,PASSWORD) VALUES(?,?)"
+//   //    res.send("this is body data")
+// });
+var sql = "SELECT USERNAME,PASSWORD FROM NODESQL";
 
-    connect.query(sinsert,[username,password],(err,data) => {
-        if (err) {
-            console.error('Error executing query:', err);
-            return res.status(500).send('Database query failed.');
-        }
-        else{
-            res.send(req.body)
-        }
-    })
+app.post("/login", (req, res) => {
     
-//    res.send("this is body data")
-})
-var port=3006
-app.listen(port,()=>{
-    console.log("Server has been created succesfully")
-})
+  connect.query(sql, (err, data) => {
+    if (err) {
+      res.send(err.message);
+    } else {
+      console.log(data);
+      for (i of data) {
+        console.log(i);
+      }
+      if (i.USERNAME === req.body.username &&  i.PASSWORD === req.body.password) {
+        res.send("Login is succesfull");
+      } else {
+        res.send("You are not registered please register");
+      }
+    }
+  });
+});
+
+var port = 3006;
+app.listen(port, () => {
+  console.log("Server has been created succesfully");
+});
